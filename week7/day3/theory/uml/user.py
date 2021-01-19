dbac69b87a839ee58d656891f3b14a6ca76f015e
gabvaztor <gabvaztor@gmail.com>
2021-01-19 12:46:42 +0100

    CW7D3 after class

--- week7/day3/theory/uml/user.py
+++ week7/day3/theory/uml/user.py
@@ -1,4 +1,4 @@
-class User:
+class User(object):
     def __init__(self, userId, password, loginStatus, registerDate):
         self.userId = userId
         self.password = password
@@ -13,11 +13,20 @@ class User:
 
 class Administrator(User):
     
-    def __init__(self, userId, password, loginStatus, registerDate):
+    def __init__(self, userId, password, loginStatus, registerDate, aN, em):
         User.__init__(self, userId, password, loginStatus, registerDate)
+        self.adminName = aN
+        self.email = em
         
+gabriel = Administrator(userId=56, password="Hola", loginStatus="True",
+                        registerDate="12/02/2020", aN="Gabriel", em="ga@jg.com")
+#gabriel.verifyLogin()
+
 class Customer(User):
     def __init__(self, eyes, user):
+        x = 2
+        _x = 4  # protegido
+        __c = 5  # privado
         User.__init__(self, 
                       user.userId,
                       user.password, 
@@ -25,13 +34,17 @@ class Customer(User):
                       user.registerDate)
         self.eyes = eyes
 
+c = Customer()
+_c = 2
+__c = 5
 class Order:
     def __init__(self, orderID, dateCreated, customer):
         self.orderID = orderID
         self.dateCreated = dateCreated
         self.customer = customer
 
-
+# Esta condición de abajo hace que si yo importo este
+# fichero desde otro módulo no se ejecuta lo que esté debajo
 if __name__ == '__main__':
     usuario = User(userId=2, 
                    password="XHG",
